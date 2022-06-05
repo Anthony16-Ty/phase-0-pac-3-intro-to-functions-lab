2104245a144910c9f7d34764ef4d0b44ff7e0a61
Anthony Kimeu <tkymeu@gmail.com>
2022-06-05 22:20:14 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+   // todo
+   return string.toUpperCase();
+ }
+ function whisper(string) {
+   // todo
+   return string.toLowerCase();
+ }
+ function logShout(string) {
+   // todo
+   console.log(string.toUpperCase());
+ }
+
+ function logWhisper(string) {
+   // todo
+   console.log(string.toLowerCase());
+ }
+ function sayHiToHeadphonedRoommate(string) {
+   // todo
+   var x= "I can't hear you!";
+   var y = "YES INDEED!";
+   var z = "I would love to!";
+   if(string.toLowerCase(string)==string){
+      return x;
+   }
+
+  else if(string.toUpperCase(string) == string){
+   return y;
+  }
+  else if("Let's have dinner together!"==string){
+      return z;
+  }
+
+  
+ }
